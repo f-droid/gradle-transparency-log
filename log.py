ed820dbdc8edbe7cd203a5179a78c9c2cfdafd9b
Hans-Christoph Steiner <hans@eds.org>
2020-05-18 12:56:33 +0200

    check gradle-wrapper.jar from local Debian's package

--- log.py
+++ log.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 #
 
+import apt
+import binascii
+import debian.debfile
+import hashlib
 import json
 import os
 import requests
@@ -48,4 +52,43 @@ for i in data:
                 fp.write(sha256)
             checksums[url[:-7]] = sha256
         write_status_codes(status_codes)
+    write_json(checksums, 'checksums.json')
+
+
+# check Debian package
+cache = apt.Cache()
+cache.open()
+package = cache.get('libgradle-core-java', None)
+if package:
+    packageFileName = os.path.basename(package.candidate.filename)
+    package.candidate.fetch_binary()
+    deb = debian.debfile.DebFile(packageFileName)
+    for filepath, listed_md5 in deb.md5sums().items():
+        filepath = filepath.decode()
+        if filepath.startswith('usr/share/java/gradle-wrapper') \
+           and filepath.endswith('.jar'):
+            d = dict()
+            md5_hasher = hashlib.md5()
+            sha256_hasher = hashlib.sha256()
+            with deb.data.get_file(filepath) as fp:
+                content = fp.read()
+            md5_hasher.update(content)
+            md5 = binascii.hexlify(md5_hasher.digest()).decode()
+            sha256_hasher.update(content)
+            sha256 = binascii.hexlify(sha256_hasher.digest()).decode()
+            if listed_md5 == md5:
+                checksums[os.path.basename(packageFileName) + ':' + filepath] = sha256
+                d['arch'] = package.architecture()
+                d['md5'] = md5
+                d['sha256'] = sha256
+                d['filepath'] = filepath
+                d['packageFileName'] = packageFileName
+                d['packageMd5'] = package.candidate.md5
+                d['packageName'] = package.name
+                d['packageSha256'] = package.candidate.sha256
+                d['packageSize'] = package.candidate.size
+                d['url'] = package.candidate.uri
+                d['version'] = package.candidate.version
+                data.append(d)
+    write_json(data, 'all.json')
     write_json(checksums, 'checksums.json')
